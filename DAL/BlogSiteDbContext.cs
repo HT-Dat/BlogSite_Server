9493d3c00fb2075d476d3cbbab9ef49f68980f13
HT-Dat <hotiendat661@gmail.com>
2023-02-17 01:46:46 +0700

    Add Category, Tag to DbContext

--- DAL/BlogSiteDbContext.cs
+++ DAL/BlogSiteDbContext.cs
@@ -19,4 +19,6 @@ public class BlogSiteDbContext : DbContext
     }
     public DbSet<Sex> Sexes { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
 }
