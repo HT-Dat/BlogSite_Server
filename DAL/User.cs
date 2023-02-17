58e82e20e0f59084a15274c05c8a8edfacec7290
HT-Dat <hotiendat661@gmail.com>
2023-02-18 01:46:39 +0700

    Add SexId field

--- DAL/User.cs
+++ DAL/User.cs
@@ -9,9 +9,13 @@ public class User
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     [Column("id", TypeName = "varchar(128)")]
     public string? Id { get; set; }
-    
+
     [Column("display_name", TypeName = "nvarchar(128)")]
     public string? DisplayName { get; set; }
+
+    [Column("sex_id")] 
+    [ForeignKey("Sex")]
+    public byte SexId { get; set; }
     
     public Sex Sex { get; set; }
 }
