e3f9ab3fe5a01f34306389207e5781569f2e3ba6
HT-Dat <hotiendat661@gmail.com>
2023-02-17 16:47:17 +0700

    Add initial migration

---
+++ DAL/Migrations/20230225083759_CreateBlogSiteDb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateBlogSiteDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sexes",
+                columns: table => new
+                {
+                    id = table.Column<byte>(type: "tinyint", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(64)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sexes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "varchar(128)", nullable: false),
+                    display_name = table.Column<string>(type: "nvarchar(128)", nullable: true),
+                    SexId = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Users_Sexes_SexId",
+                        column: x => x.SexId,
+                        principalTable: "Sexes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SexId",
+                table: "Users",
+                column: "SexId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Sexes");
+        }
+    }
+}
