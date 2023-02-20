434a1bf4c11054e75cfab3c10a5862deb943b54b
HT-Dat <hotiendat661@gmail.com>
2023-02-21 02:27:13 +0700

    Add self-reference, time fields

--- DAL/Post.cs
+++ DAL/Post.cs
@@ -5,6 +5,11 @@ namespace DAL;
 
 public class Post
 {
+    public Post()
+    {
+        ChildPosts = new HashSet<Post>();
+    }
+
     [Key]
     [Column("id",TypeName = "int")]
     public int Id { get; set; }
@@ -14,6 +19,22 @@ public class Post
     public int AuthorId { get; set; }
     public User Author { get; set; }
     
+    [ForeignKey("Parent")]
+    [Column("parent_id", TypeName = "int")]
+    public int? ParentId { get; set; }
+    public Post ParentPost { get; set; }
     
+    [Column("created_date", TypeName = "datetime2(7)")]
+    public DateTime CreatedDate { get; set; }
+    
+    [Column("updated_date", TypeName = "datetime2(7)")]
+    public DateTime UpdatedDate { get; set; }
+    
+    [Column("published_date", TypeName = "datetime2(7)")]
+    public DateTime PublishedDate { get; set; }
+    
+    
+    
+    public virtual ICollection<Post> ChildPosts { get; set; }
     
 }
