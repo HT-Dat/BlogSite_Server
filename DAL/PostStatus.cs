7db23aa98571787b1d2fb73ea78c24ab3606c2af
HT-Dat <hotiendat661@gmail.com>
2023-02-26 02:49:51 +0700

    Add Status lookup table

--- DAL/PostStatus.cs
+++ DAL/PostStatus.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DAL;
 
 public class PostStatus
 {
+    [Key]
+    [Column("id", TypeName = "tinyint")]
+    public byte Id { get; set; }
     
+    [Column("name", TypeName = "nvarchar(64)")]
+    public string Name { get; set; }
+    
+    public ICollection<Post> Posts { get; set; }
 }
