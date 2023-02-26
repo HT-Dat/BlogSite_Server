2db01dbf4bca007c58eb615277b19980ccdaf9bc
HT-Dat <hotiendat661@gmail.com>
2023-02-27 03:07:55 +0700

    Add properties to PostCategory.cs

--- DAL/PostCategory.cs
+++ DAL/PostCategory.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace DAL;
 
 public class PostCategory
 {
+    [ForeignKey("Category")]
+    [Column("category_id", TypeName = "int")]
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
     
+    [ForeignKey("Post")]
+    [Column("post_id",TypeName = "int")]
+    public int PostId { get; set; }
+    public Post Post { get; set; }
 }
