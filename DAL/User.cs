be09fa8a7afdd4cc4d60a7f36f8482fa0375a22b
HT-Dat <hotiendat661@gmail.com>
2023-02-28 17:18:13 +0700

    Add table name and Post list to User.cs

--- DAL/User.cs
+++ DAL/User.cs
@@ -2,16 +2,16 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL;
-
+[Table("User")]
 public class User
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     [Column("id", TypeName = "varchar(128)")]
-    public string? Id { get; set; }
+    public string Id { get; set; }
 
     [Column("display_name", TypeName = "nvarchar(128)")]
-    public string? DisplayName { get; set; }
+    public string DisplayName { get; set; }
 
     [Column("sex_id")] 
     [ForeignKey("Sex")]
@@ -20,4 +20,5 @@ public class User
     public Sex Sex { get; set; }
 
     public ICollection<Comment> Comments { get; set; }
+    public ICollection<Post> Posts { get; set; }
 }
