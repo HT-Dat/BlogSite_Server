399770f0ae455ba1ce24a6708779537bed0ce3f8
HT-Dat <hotiendat661@gmail.com>
2023-02-28 17:20:33 +0700

    Fix ParentComment from Post to Comment, AuthorId from int to string, add Comment list for self-referencing

--- DAL/Comment.cs
+++ DAL/Comment.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DAL;
+[Table("Comment")]
 
 public class Comment
 {
@@ -10,23 +11,26 @@ public class Comment
     public int Id { get; set; }
     
     [ForeignKey("Author")]
-    [Column("author_id",TypeName = "int")]
-    public int AuthorId { get; set; }
+    [Column("author_id",TypeName = "varchar(128)")]
+    public string AuthorId { get; set; }
     public User Author { get; set; }
     
     [ForeignKey("ParentComment")]
-    [Column(nameof(ParentComment), TypeName = "int")]
+    [Column("parent_id", TypeName = "int")]
     public int? ParentId { get; set; }
-    public Post ParentComment { get; set; }
+    public Comment ParentComment { get; set; }
     
     [Column("posted_date",TypeName = "datetime2(7)")]
     public DateTime PostedDate { get; set; }
     
     [Column("content", TypeName = "nvarchar(max)")]
     public string Content { get; set; }
-
+    
     [ForeignKey(nameof(Post))]
     [Column("post_id",TypeName = "int")]
     public int PostId { get; set; }
     public Post Post { get; set; }
+    
+    public ICollection<Comment> ChildComments { get; set; }
+    
 }
