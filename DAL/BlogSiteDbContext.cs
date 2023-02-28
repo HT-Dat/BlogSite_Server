cdf0303a66e5fec71bbb7af248a44448a2b51853
HT-Dat <hotiendat661@gmail.com>
2023-02-28 17:16:23 +0700

    Add Comment, Post, PostCategory, PostStatus, PostTag list to DbContext

--- DAL/BlogSiteDbContext.cs
+++ DAL/BlogSiteDbContext.cs
@@ -21,4 +21,10 @@ public class BlogSiteDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Tag> Tags { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<PostCategory> PostCategories { get; set; }
+    public DbSet<PostStatus> PostStatusEnumerable { get; set; }
+    public DbSet<PostTag> PostTags { get; set; }
+    
 }
