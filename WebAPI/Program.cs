458ec7e5b3db0915668c2c5605ac5bdafcde383d
HT-Dat <hotiendat661@gmail.com>
2023-03-05 17:33:26 +0700

    Add init Firebase condition

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -13,25 +13,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-FirebaseApp.Create(new AppOptions()
-{
-    Credential = GoogleCredential.FromJson(Environment.GetEnvironmentVariable("FIREBASE_SECRET")),
-});
+
 builder.Services.AddDbContext<BlogSiteDbContext>(
     options => options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING")));
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+// Firebase init will cause error on ef migrations because missing env like FIREBASE_SECRET which made the API to crash
+// Need to comment this section before doing anythings related to ef migrations
+if (Environment.GetEnvironmentVariable("FIREBASE_SECRET").IsNullOrEmpty() == false)
 {
-    options.Authority = "https://securetoken.google.com/hotiendat-blog";
-    options.TokenValidationParameters = new TokenValidationParameters
+    FirebaseApp.Create(new AppOptions()
     {
-        ValidateIssuer = true,
-        ValidIssuer = "https://securetoken.google.com/hotiendat-blog",
-        ValidateAudience = true,
-        ValidAudience = "hotiendat-blog",
-        ValidateLifetime = true,
-    };
-});
+        Credential = GoogleCredential.FromJson(Environment.GetEnvironmentVariable("FIREBASE_SECRET")),
+    });
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    {
+        options.Authority = "https://securetoken.google.com/hotiendat-blog";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "https://securetoken.google.com/hotiendat-blog",
+            ValidateAudience = true,
+            ValidAudience = "hotiendat-blog",
+            ValidateLifetime = true,
+        };
+    });
+}
 
+// End of Init Firebase Auth
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
