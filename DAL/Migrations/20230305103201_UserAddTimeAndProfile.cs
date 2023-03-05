2ca57dd7cf1102405f3ce6e004184833242b9895
HT-Dat <hotiendat661@gmail.com>
2023-03-05 17:35:04 +0700

    Migrate update User table

---
+++ DAL/Migrations/20230305103201_UserAddTimeAndProfile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserAddTimeAndProfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "created_date",
+                table: "User",
+                type: "datetime2(7)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "intro",
+                table: "User",
+                type: "nvarchar(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_login",
+                table: "User",
+                type: "datetime2(7)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "profile",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "created_date",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "intro",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "last_login",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "profile",
+                table: "User");
+        }
+    }
+}
