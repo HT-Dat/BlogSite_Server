e4d56ccbc9fa113cdc3721a2a3775d782d6e2b47
HT-Dat <hotiendat661@gmail.com>
2023-03-13 19:01:01 +0700

    Check and return register status for auth service

--- BLL/Services/AuthService.cs
+++ BLL/Services/AuthService.cs
@@ -6,13 +6,34 @@ namespace BLL.Services;
 
 public class AuthService : IAuthService
 {
-    private BlogSiteDbContext _blogSiteDbContext;
+    private readonly string REGISTERED_STATUS = "registered";
+    private readonly string UNREGISTERED_STATUS = "unregistered";
+    private readonly BlogSiteDbContext _blogSiteDbContext;
+
+    public AuthService(BlogSiteDbContext blogSiteDbContext)
+    {
+        _blogSiteDbContext = blogSiteDbContext;
+    }
+
     public async Task<UserRegistrationStatusReturnDto> VerifyAccess(string uid)
     {
+        string message;
+        string status;
+        if (_blogSiteDbContext.Users.Any(o => o.Id == uid))
+        {
+            status = REGISTERED_STATUS;
+            message = "The current user is registered";
+
+        }
+        else
+        {
+            status = UNREGISTERED_STATUS;
+            message = "The current user is not registered";
+        }
         return new UserRegistrationStatusReturnDto
         {
-            Message = "abc",
-            Status = "cde"
+            Message = message,
+            Status = status
         };
     }
 }
