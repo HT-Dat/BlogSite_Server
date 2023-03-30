805e5ee269dbec0ae530cf3b1e501bbae4af7515
HT-Dat <hotiendat661@gmail.com>
2023-03-31 03:33:11 +0700

    Update in PostService now only update non null prop

--- BLL/Services/PostService.cs
+++ BLL/Services/PostService.cs
@@ -1,8 +1,9 @@
 using AutoMapper;
 using BLL.Services.IServices;
-using DAL;
+using DAL.Persistence;
 using DAL.Entities;
 using DTO.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace BLL.Services;
 
@@ -23,14 +24,23 @@ public class PostService : IPostService
         return _mapper.Map<PostToReturnDto>(post);
     }
 
+    public async Task<List<PostToReturnForListDto>> GetList()
+    {
+        var listPostFromDb = await _blogSiteDbContext.Posts.ToListAsync();
+        var mappedList = _mapper.Map<List<Post>, List<PostToReturnForListDto>>(listPostFromDb);
+        return mappedList;
+    }
+
     public async Task<Post> Add(string authorId)
     {
         var addedPost = new Post
         {
-            Content = String.Empty,
+            Content = string.Empty,
             Title = string.Empty,
             AuthorId = authorId,
-            CreatedDate = DateTime.UtcNow
+            CreatedDate = DateTime.UtcNow,
+            UpdatedDate = DateTime.UtcNow,
+            StatusId = 0
         };
         await _blogSiteDbContext.Posts.AddAsync(addedPost);
         await _blogSiteDbContext.SaveChangesAsync();
@@ -46,11 +56,27 @@ public class PostService : IPostService
     {
         var updatingPost = _mapper.Map<Post>(postToUpdate);
         updatingPost.UpdatedDate = DateTime.UtcNow;
-        _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.Content).IsModified = true;
-        _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.Title).IsModified = true;
-        _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.StatusId).IsModified = true;
-        _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.UpdatedDate).IsModified = true;
-        
+
+        foreach (var propInfo in typeof(Post).GetProperties())
+        {
+            var postPropValue = propInfo.GetValue(updatingPost);
+            if (postPropValue != null && propInfo.Name != "Id")
+            {
+                if (postPropValue is string)
+                {
+                    _blogSiteDbContext.Entry<Post>(updatingPost).Property(propInfo.Name).IsModified = true;
+                }
+                else if (postPropValue is DateTime time && time != default(DateTime))
+                {
+                    _blogSiteDbContext.Entry<Post>(updatingPost).Property(propInfo.Name).IsModified = true;
+                }
+            }
+        }
+        // _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.Content).IsModified = true;
+        // _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.Title).IsModified = true;
+        // _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.StatusId).IsModified = true;
+        // _blogSiteDbContext.Entry<Post>(updatingPost).Property(o => o.UpdatedDate).IsModified = true;
+
         await _blogSiteDbContext.SaveChangesAsync();
         return _mapper.Map<PostToReturnDto>(updatingPost);
     }
