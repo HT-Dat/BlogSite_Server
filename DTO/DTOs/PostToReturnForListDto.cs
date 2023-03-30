afec33acdf814be216c215e746e1b949827535d8
HT-Dat <hotiendat661@gmail.com>
2023-03-31 03:32:39 +0700

    Init PostToReturnForListDto.cs

---
+++ DTO/DTOs/PostToReturnForListDto.cs
@@ -0,0 +1,9 @@
+namespace DTO.DTOs;
+
+public class PostToReturnForListDto
+{
+    public int Id { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public byte? StatusId { get; set; }
+    public string Title { get; set; }
+}
