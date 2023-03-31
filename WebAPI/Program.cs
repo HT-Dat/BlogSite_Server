d99aa0bd9106dbab3d4ec3091e6464d9a2bf13cf
HT-Dat <hotiendat661@gmail.com>
2023-04-01 04:43:31 +0700

    Update cors rule, inject storageClient

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -4,6 +4,7 @@ using BLL.Services.IServices;
 using DAL.Persistence;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -11,7 +12,7 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,9 +24,10 @@ builder.Services.AddDbContext<BlogSiteDbContext>(
     options => options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING")));
 if (Environment.GetEnvironmentVariable("FIREBASE_SECRET").IsNullOrEmpty() == false)
 {
+    var cred = GoogleCredential.FromJson(Environment.GetEnvironmentVariable("FIREBASE_SECRET"));
     FirebaseApp.Create(new AppOptions()
     {
-        Credential = GoogleCredential.FromJson(Environment.GetEnvironmentVariable("FIREBASE_SECRET")),
+        Credential = cred,
     });
     builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
     {
@@ -39,6 +41,8 @@ if (Environment.GetEnvironmentVariable("FIREBASE_SECRET").IsNullOrEmpty() == fal
             ValidateLifetime = true,
         };
     });
+    var storageClient = StorageClient.Create(cred);
+    builder.Services.AddSingleton(storageClient);
 }
 
 // End of Init Firebase Auth
@@ -63,10 +67,11 @@ app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
-app.UseCors(options =>
-{
-    options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-});
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
 
 app.MapControllers();
 
